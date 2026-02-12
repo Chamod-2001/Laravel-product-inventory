ecda760bf2f66c76192a780ba5828b894b677728
D. L. Chamod Lakshitha <132426976+Chamod-2001@users.noreply.github.com>
2026-02-12 14:09:07 +0530

    feat(product): add product controller and resource routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+use App\Http\Controllers\ProductController;
+// Route::resource create CRUD auto and connect Product contorller calss
+Route::resource('products', ProductController::class);
